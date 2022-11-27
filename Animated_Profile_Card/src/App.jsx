f89196381637f676cc0ba9037c9148ca8c8708a9
Akshay-Gole <akshaygole01111@gmail.com>
2022-11-27 16:39:18 +0530

    50% of cards

---
+++ Animated_Profile_Card/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.scss'
+
+function App() {
+  return (
+    <div className='card'>
+      <div className="lines"></div>
+      <div className="img-box">
+        <img src="../src/img/IMG_1795.JPG" alt="Akshay Gole" />
+      </div>
+      <div className="content">
+        <div className="details">
+          <h2>Akshay Gole<span>Intern Frontend Dev</span></h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
